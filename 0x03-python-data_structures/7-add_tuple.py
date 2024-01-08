49827457f5ea87df0e160f7b65c55b63e4cab664
ahmedtiko <ahmedmmtarek@gmail.com>
2024-01-08 05:40:53 -0800

    auto push

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two elements of each tuple (or use 0 if not present)
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
+   " # Extract the first two elements of each tuple (or use 0 if not present)
     a1, a2 = tuple_a[:2] + (0, 0)
     b1, b2 = tuple_b[:2] + (0, 0)
 
     # Perform addition and return the result as a tuple
     result_tuple = (a1 + b1, a2 + b2)
-    return result_tuple
+    return result_tuple"
